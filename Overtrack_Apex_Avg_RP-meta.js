60ba760151cf0916c49670dca68e33b5aef55f52
Elementofprgress <elementofprgress@gmail.com>
2020-09-17 22:12:08 -0700

    update to point to release
    update to point to release

--- Overtrack_Apex_Avg_RP-meta.js
+++ Overtrack_Apex_Avg_RP-meta.js
@@ -1,18 +1,5 @@
 // ==UserScript==
-// @name        Apex RP Overtrack
-// @description Calculate RP & Avg RP based on stats available.
-// @author      Elementofprgress
-// @icon        https://cdn.overtrack.gg/static/images/favicon.png
-// @version     1.2
-// @updateURL   https://raw.githubusercontent.com/elementofprgress/OvertrackApexAvgRP/master/Overtrack_Apex_Avg_RP-meta.js
-// @downloadURL https://raw.githubusercontent.com/elementofprgress/OvertrackApexAvgRP/master/Overtrack_Apex_Avg_RP.js
-// @match       *overtrack.gg/apex/games?season=*&ranked=true*
-// @connect     *
-// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
-// @grant       GM_getValue
-// @grant       GM_setValue
-// @grant       GM_xmlhttpRequest
-// @grant       GM_info
-// @grant GM_xmlhttpRequest
-// @grant GM_addStyle
+// @version    1.0.2
+// @name    Apex RP Overtrack
+// @namespace    https://github.com/elementofprgress
 // ==/UserScript==
--- Overtrack_Apex_Avg_RP.js
+++ Overtrack_Apex_Avg_RP.js
@@ -3,18 +3,19 @@
 // @description Calculate RP & Avg RP based on stats available.
 // @author      Elementofprgress
 // @icon        https://cdn.overtrack.gg/static/images/favicon.png
-// @version     1.2
-// @updateURL   https://raw.githubusercontent.com/elementofprgress/OvertrackApexAvgRP/master/Overtrack_Apex_Avg_RP-meta.js
-// @downloadURL https://raw.githubusercontent.com/elementofprgress/OvertrackApexAvgRP/master/Overtrack_Apex_Avg_RP.js
+// @version     1.0.2
+// @namespace   https://github.com/elementofprgress
+// @updateURL   https://raw.githubusercontent.com/elementofprgress/OvertrackApexAvgRP/master/downloads/Overtrack_Apex_Avg_RP-meta.js
+// @downloadURL https://raw.githubusercontent.com/elementofprgress/OvertrackApexAvgRP/master/downloads/Overtrack_Apex_Avg_RP.js
 // @match       *overtrack.gg/apex/games?season=*&ranked=true*
 // @connect     *
-// @require https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       GM_xmlhttpRequest
 // @grant       GM_info
-// @grant GM_xmlhttpRequest
-// @grant GM_addStyle
+// @grant       GM_xmlhttpRequest
+// @grant       GM_addStyle
 // ==/UserScript==
 (function(){
     if ($("#gamesListBody")[0].children[2].lastElementChild.className === "rp-delta")
